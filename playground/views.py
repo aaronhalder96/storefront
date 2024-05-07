255a6f8e53c039970b3597efa9ac32b7a40dabe3
aaronhalder-wisecode <aaron@wisecodetech.com>
2024-05-07 17:54:19 +0600

    initial commit

---
+++ playground/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
+import requests
+
+# Create your views here.
+class SayHello(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': 'Aaron'})
---
+++ store/tests/test_collections.py
@@ -0,0 +1,56 @@
+from rest_framework import status
+import pytest
+from django.contrib.auth.models import User
+from model_bakery import baker
+from store.models import Collection
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+    return do_create_collection
+
+@pytest.fixture
+def retrieve_collection(api_client):
+    def do_retrieve_collection(collection_id):
+        return api_client.get(f'/store/collections/{collection_id}/')
+    return do_retrieve_collection
+
+@pytest.mark.django_db
+class TestCreateCollection:
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
+        response = create_collection({'title': 'a'})
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
+        authenticate(is_staff=False)
+        response = create_collection({'title': 'a'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+        response = create_collection({'title': ''})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+        response = create_collection({'title': 'a'})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, retrieve_collection):
+        collection = baker.make(Collection)
+        response = retrieve_collection(collection.id)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
+
+    def test_if_collection_not_exists_returns_404(self, retrieve_collection):
+        response = retrieve_collection(100)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
